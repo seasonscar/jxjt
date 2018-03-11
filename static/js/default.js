e25bf6d4de9649dc380d6990250c69d2a5816f10
seasonscar <525744150@qq.com>
2018-03-11 13:26:32 +0800

    表单Beta1.0版

--- static/js/default.js
+++ static/js/default.js
@@ -1,7 +1,14 @@
 function searchText(searchText, ulList) {
     if(searchText != "") {
+        var searchText = searchText.toLowerCase();
         ulList.find("li").hide();
-        ulList.find("a:contains(" + searchText + ")").parent().show();
+        for(var i=0; i<ulList.find("li").length; i++) {
+            var liTxt = ulList.find("li").eq(i).find("a").text().toLowerCase();
+            console.log(liTxt);
+            if(liTxt.indexOf(searchText) > -1) {
+                ulList.find("li").eq(i).show();
+            }
+        }
     } else {
         ulList.find("li").show();
     }
